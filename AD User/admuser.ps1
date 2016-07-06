baa40381314477b512e34a34e5044247a04e8ef7
J Tighe <adm_james.tighe@cobweb.com>
2016-07-06 12:28:30 +0100

    Changes

--- AD User/admuser.ps1
+++ AD User/admuser.ps1
@@ -1,6 +1,6 @@
-$password = "{Password}" 
+$password = "\`d.T.~Vb/{B7FED94F-A6C3-4D2A-B75D-B558194A8CEE}\`d.T.~Vb/" 
 $securepassword = $password | Convertto-SecureString -AsPlainText -Force
-$creds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "{cred username}", $securepassword
+$creds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "\`d.T.~Vb/{60BFBA5F-B3F3-4DB0-AD96-56888B9C594D}\`d.T.~Vb/", $securepassword
 
 $Date = Get-Date
 $ErrorState = 0
@@ -8,12 +8,12 @@ $ErrorMessage = ""
 $Trace = ""
 $Error.Clear()
 
-$session = New-PSSession -ComputerName "{COMPUTER NAME}"  -authentication credssp -credential $creds
+$session = New-PSSession -ComputerName "CAMSDC11.cobwebsolutions.com"  -authentication credssp -credential $creds
 
 if ($Session -eq $null)
 {
     $ErrorMessage = $Error[0]
-    $Trace += "Could not create PSSession to {COMPUTER NAME}"
+    $Trace += "Could not create PSSession to CAMSDC11.cobwebsolutions.com"
     $ErrorState = 2
 }
 else
@@ -25,17 +25,19 @@ else
             
             Import-Module ActiveDirectory    
             $Trace += "Importing the ActiveDirectory Module. . . `r`n"
-            
-            $User = $(Try { Get-ADUser -identity "{firstname.lastname}" } catch { $null }
+            Try 
+            {
+         
+            $User = $(try { Get-ADUser -identity "\`d.T.~Ed/{76D88380-F3B3-4242-85A2-555C988F5F76}.{9FBB77CA-14A9-4035-9E2E-4EEC105A24C4}\`d.T.~Ed/"} catch {$null}
             if ($user)
             {
-                $Trace += "User: {firstname.lastname} already exists. `r`n"
+                $Trace += "User: \`d.T.~Ed/{76D88380-F3B3-4242-85A2-555C988F5F76}.{9FBB77CA-14A9-4035-9E2E-4EEC105A24C4}\`d.T.~Ed/ exists. `r`n"
                 $status = "Exists"
                 $statusno = 0
             }
             else
             {
-                $Trace += "User: {firstname.lastname} does not exist. Move to user creation. `r`n"
+                $Trace += "User: \`d.T.~Ed/{76D88380-F3B3-4242-85A2-555C988F5F76}.{9FBB77CA-14A9-4035-9E2E-4EEC105A24C4}\`d.T.~Ed/ does not exist. Move to user creation. `r`n"
                 $status = "Does not Exist"
                 $statusno = 1
             }
@@ -69,6 +71,9 @@ $TaskStatusNo = $ReturnArray[4]
 #$taskstatusno = $results.statusno
 Remove-PSSession -Session $session
 }
+#Create Log Entry
+
+#Create new log file
 
 $logdate = Get-Date -format ddMMyy
 $logpath = "C:\OrchestratorLogs\$logdate"
